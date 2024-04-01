976b1241be2a9ceffdedc1eb6544a28b4dca8b7a
Serhii Ternovtsov <tserg42@gmail.com>
2024-04-01 23:23:36 +0300

    fix formatting

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,6 @@
 pub(crate) mod health_check;
 pub(crate) mod subscriptions;
-pub (crate) mod subscriptions_confirm;
+pub(crate) mod subscriptions_confirm;
 pub use health_check::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
