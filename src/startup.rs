9c43d4ba78a4bb8525cb2296b2f6c7a6d052502b
Serhii Ternovtsov <tserg42@gmail.com>
2024-05-08 01:46:42 +0300

    Protect login form fronm XSS

--- src/startup.rs
+++ src/startup.rs
@@ -5,6 +5,7 @@ use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
 use actix_web::{dev::Server, web, App, HttpServer};
+use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -12,11 +13,15 @@ use tracing_actix_web::TracingLogger;
 
 pub struct ApplicationBaseUrl(pub String);
 
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
+
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     // Wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
@@ -33,9 +38,10 @@ pub fn run(
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
             // Register the connection as part of the application state
-            .app_data(db_pool.clone())
+        .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
@@ -70,8 +76,9 @@ impl Application {
         let server = run(
             listener,
             connection_pool,
-            email_client, // New parameter!
+            email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
         Ok(Self { port, server })
     }
