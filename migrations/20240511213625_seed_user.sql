f076d9834d26c84a7c83ebe06be9c132c89ccce2
Serhii Ternovtsov <tserg42@gmail.com>
2024-05-12 01:10:58 +0300

    Add seed user

---
+++ migrations/20240511213625_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'c916ec58-90ec-4483-ae66-ec7db3799047',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$/dYpkcfUJE4wekw09efZjg$IAAHPgxWjeHN4NzA+OMwro2uVcevZ+F//LP+ZgDwvHw'
+);
