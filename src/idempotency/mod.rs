627937a3c557620c9096de39fc15f3280c4e2e4a
Serhii Ternovtsov <tserg42@gmail.com>
2024-05-17 00:25:09 +0300

    Serialize concurrent publishing of same newsletter

--- src/idempotency/mod.rs
+++ src/idempotency/mod.rs
@@ -1,5 +1,4 @@
 mod key;
 pub use key::IdempotencyKey;
 mod persistence;
-pub use persistence::get_saved_response;
-pub use persistence::save_response;
+pub use persistence::{get_saved_response, save_response, try_processing, NextAction};
