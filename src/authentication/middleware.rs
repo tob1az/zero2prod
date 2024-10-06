63e918e27c1e5c7a987cfc519fa9badbe3ac8874
Serhii Ternovtsov <tserg42@gmail.com>
2024-10-07 00:39:56 +0300

    Fix build

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -5,7 +5,7 @@ use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::error::InternalError;
 use actix_web::FromRequest;
 use actix_web::HttpMessage;
-use actix_web_lab::middleware::Next;
+use actix_web::middleware::Next;
 use std::ops::Deref;
 use uuid::Uuid;
 
